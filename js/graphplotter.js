f156477e810d8bc2dd1317b317ae232bb2537954
harryggg <ma.tanghao@dhs.sg>
2016-03-01 12:09:07 +0800

    add comment to chart plotter

--- js/graphplotter.js
+++ js/graphplotter.js
@@ -1,3 +1,4 @@
+/** process raw string data and plot a graph out of it*/
 GraphiteGraphPlotter = function(div) {
     this.div = div;
     this.chart;
@@ -7,63 +8,80 @@ GraphiteGraphPlotter = function(div) {
     this.xVal = 0;
     this.dataLength = 50;
     this.dataparser = new GraphiteDataParser();
+
+    /////////////////////
+    //PROTECTED METHODS//
+    /////////////////////
+    /**
+     * initialize the graph
+     * @params {number} dataNumber - number of data per line
+     * @params {boolean} withXCord - whether the first number of the line is the x-coordinate
+     */
+    this.initGraph = function(dataNumber, withXCord) {
+        this.xVal = 0;
+        this.dataNumber = dataNumber || 1;
+        this.withXCord = (typeof withXCord !== 'undefined') ? withXCord : false;
+        var dataStartPos = 0;
+        if (withXCord) {
+            dataStartPos = 1;
+        }
+        this.dps = fillArray([], dataNumber);
+        var tempData = [];
+        for (var i = dataStartPos; i < dataNumber; i++) {
+            tempData.push({
+                type: "line",
+                name: "data" + i,
+                dataPoints: this.dps[i]
+            });
+        }
+        this.chart = new CanvasJS.Chart(this.div, {
+            title: {
+                text: "geia sou kosmos"
+            },
+            data: tempData
+        });
+    }
+
+    /**
+     * update the chart using list of data
+     * @params {array} datalist - number of data per line
+     */
+    this.updateChart = function(datalist) {
+        for (var i = 0; i < datalist.length; i++) {
+            if (datalist[i].length != this.dataNumber) continue;
+            var dataStartPos = 0;
+            var xCordValue = this.xVal;
+            if (this.withXCord) {
+                dataStartPos = 1;
+                xCordValue = datalist[i][0];
+            }
+            for (var j = dataStartPos; j < this.dps.length; j++) {
+                this.dps[j].push({
+                    x: xCordValue,
+                    y: datalist[i][j]
+                });
+                if (this.dps[j].length > this.dataLength) {
+                    this.dps[j].shift();
+                }
+            }
+            this.xVal++;
+        };
+        this.chart.render();
+    };
+
     this.initGraph();
 }
 
+/**
+ * add raw data into chart 
+ * @params {string} rawData - string of unprocessed data
+ */
 GraphiteGraphPlotter.prototype.addNewData = function(rawData) {
     this.dataparser.addRawData(rawData);
     if ((this.dataNumber != this.dataparser.getdataNumber()) || (this.withXCord != this.dataparser.isWithXCord())) {
         this.initGraph(this.dataparser.getdataNumber(), this.dataparser.isWithXCord());
     }
     this.updateChart(this.dataparser.showNewData());
-}
-
-GraphiteGraphPlotter.prototype.updateChart = function(datalist) {
-    for (var i = 0; i < datalist.length; i++) {
-        if (datalist[i].length != this.dataNumber) continue;
-        var dataStartPos = 0;
-        var xCordValue = this.xVal;
-        if (this.withXCord) {
-            dataStartPos = 1;
-            xCordValue = datalist[i][0];
-        }
-        for (var j = dataStartPos; j < this.dps.length; j++) {
-            this.dps[j].push({
-                x: xCordValue,
-                y: datalist[i][j]
-            });
-            if (this.dps[j].length > this.dataLength) {
-                this.dps[j].shift();
-            }
-        }
-        this.xVal++;
-    };
-    this.chart.render();
-};
-
-GraphiteGraphPlotter.prototype.initGraph = function(dataNumber, withXCord) {
-    this.xVal = 0;
-    this.dataNumber = dataNumber || 1;
-    this.withXCord = (typeof withXCord !== 'undefined') ? withXCord : false;
-    var dataStartPos = 0;
-    if (withXCord) {
-        dataStartPos = 1;
-    }
-    this.dps = fillArray([], dataNumber);
-    var tempData = [];
-    for (var i = dataStartPos; i < dataNumber; i++) {
-        tempData.push({
-            type: "line",
-            name: "data" + i,
-            dataPoints: this.dps[i]
-        });
-    }
-    this.chart = new CanvasJS.Chart(this.div, {
-        title: {
-            text: "geia sou kosmos"
-        },
-        data: tempData
-    });
 }
 
 function fillArray(content, amount) {
